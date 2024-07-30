543e7ccbf6ea961fce893e7d3050b93ff5b13cd1
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2024-07-31 08:03:02 +0900

    feat: router

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::IntoResponse, routing::post, Router};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let app = Router::new().route("/", post(handle_broadcast_message));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+async fn handle_broadcast_message() -> impl IntoResponse {}
